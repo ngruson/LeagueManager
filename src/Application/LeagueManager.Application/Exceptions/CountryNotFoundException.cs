51d89049032ef1ecc5af6e0ad56c5510e45ff2e5
Nils Gruson <ngruson@hotmail.com>
2020-06-11 20:44:55 +0200

    Added back CountryNotFoundException

---
+++ src/Application/LeagueManager.Application/Exceptions/CountryNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace LeagueManager.Application.Exceptions
+{
+    public class CountryNotFoundException : LeagueManagerException
+    {
+        public CountryNotFoundException() { }
+
+        public CountryNotFoundException(string name) : base($"Country \"{name}\" not found.")
+        {
+        }
+
+        protected CountryNotFoundException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
